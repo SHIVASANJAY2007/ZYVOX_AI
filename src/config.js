8bd3e4f34ef2288b534f7be6f22b8c817dcb2f87
SHIVA SANJAY.N.D <sanjudragon2007@gmail.com>
2026-08-14 15:38:49 +0530

    Chatbot DONE !!!

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 
-export const N8N_WEBHOOK_URL = import.meta.env.VITE_N8N_WEBHOOK_URL || "https://replication-submitted-setting-owned.trycloudflare.com/webhook/591a4f49-ef7f-443f-9374-13120ae3dc94";
+export const N8N_WEBHOOK_URL = import.meta.env.VITE_N8N_WEBHOOK_URL || "https://pumps-funeral-connection-tyler.trycloudflare.com/webhook/591a4f49-ef7f-443f-9374-13120ae3dc94";
